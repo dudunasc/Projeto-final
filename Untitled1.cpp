edf088903b700965fdb104c6fc3d0b1e052c809b
Luiz Eduardo <eduardonoliveira13@gmail.com>
2023-11-27 17:40:15 -0300

    dando inicio ao código
    iniciei o código

---
+++ Untitled1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	
+
+
+	char login[50];
+	char senha[50];
+		
+		printf("Login: \n");
+		gets("%s", &login);
+		printf("Senha: \n");
+		scanf("%s", &senha);
+
+}
